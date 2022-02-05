586d7ca2e736c38270866bbc7655cf704fa22340
David Amador <54488581+dsamador@users.noreply.github.com>
2022-02-05 09:42:54 -0500

    Classes and global imports

---
+++ BlazorApp/Client/Helpers/UtilidadesString.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorApp.Client.Helpers
+{
+    public class UtilidadesString
+    {
+        public static string Enmayuscular(string valor) => valor.ToUpper();
+    }
+}
---
+++ BlazorApp/Shared/Entidades/Pelicula.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Shared.Entidades
+{
+    public class Pelicula
+    {
+        public string Titulo { get; set; }
+        public DateTime FechaLanzamiento { get; set; }
+    }
+}
